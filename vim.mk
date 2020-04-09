98ce9421323a50f45ee5365354a4343f5862bf64
Omer Yehudai <omer.yehudai@quadream.io>
2020-04-09 22:15:57 +0300

    added plugins install also

--- vim.mk
+++ vim.mk
@@ -4,13 +4,16 @@ NVIM_CONFIG=${HOME}/.config/nvim
 
 setup: setup-nvim
 
-setup-nvim: install-nvim $(NVIM_PLUG) link-nviminit
+setup-nvim: install-nvim $(NVIM_PLUG) link-nviminit install-plugins
 
 install-nvim: $(PACAPT)
 	$(call pacapt, install neovim)
 
 link-nviminit: $(NVIM_CONFIG)
 	ln -s ${CURDIR}/init.vim $(NVIM_CONFIG)
+
+install-plugins:
+	nvim --headless +PlugInstall +qall
 
 $(NVIM_PLUG):
 	curl -fLo $@ --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
