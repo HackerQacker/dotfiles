85f14bd6c86205f5dce11ab07ea7a54411ca95db
OmerYe <omeryehudai@gmail.com>
2022-04-28 18:08:33 +0300

    Leaving oh-my-zsh for antidote+starship

---
+++ .zprofile
@@ -0,0 +1,3 @@
+
+export PATH="$HOME/.cargo/bin:$PATH"
+eval "$(pyenv init --path)"
--- zsh.mk
+++ zsh.mk
@@ -2,7 +2,7 @@ export OMZSH_PATH=${HOME}/.oh-my-zsh
 
 setup: setup-zsh
 
-setup-zsh: $(ZSH) $(OMZSH_PATH) link-zsh-env
+setup-zsh: $(ZSH) link-zsh-env link-zsh-plugins link-starship-toml
 setup-zsh: SH_NAME=$(shell which zsh) 
 setup-zsh: 
 	$(call maybesudo, chsh -s $(SH_NAME) $(shell whoami))
@@ -11,13 +11,23 @@ ZSH = $(shell which zsh)
 $(ZSH): $(PACAPT)
 	$(call pacapt, install zsh)
 
-$(OMZSH_PATH):
-	$(CURDIR)/install_omzsh.sh
+# $(OMZSH_PATH):
+# 	$(CURDIR)/install_omzsh.sh
 
 link-zsh-env:
+	$(call backup, ${HOME}/.zprofile)
+	ln -s -f ${CURDIR}/.zprofile ${HOME}
 	$(call backup, ${HOME}/.zshrc)
-	# -f bacause installation of oh-my-zsh already creates one
 	ln -s -f ${CURDIR}/.zshrc ${HOME}
 	$(call backup, ${HOME}/.zshenv)
 	ln -s -f ${CURDIR}/.zshenv ${HOME}
+
+link-zsh-plugins:
+	$(call backup, ${HOME}/.zsh_plugins.txt)
+	ln -s -f ${CURDIR}/.zsh_plugins.txt ${HOME}
+
+link-starship-toml:
+	mkdir -p ${HOME}/.config
+	$(call backup, ${HOME}/.config/starship.toml)
+	ln -s -f ${CURDIR}/starship.toml ${HOME}/.config
 
