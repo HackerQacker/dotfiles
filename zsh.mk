1be186882eb31d7b63b69fab6b26835fbcee9da4
OmerYe <omeryehudai@gmail.com>
2022-07-05 12:12:21 +0300

    fix

--- zsh.mk
+++ zsh.mk
@@ -1,4 +1,9 @@
 
+ZSH = $(shell which zsh)
+PURE = ${HOME}/.zsh/pure
+ANTIDOTE = ${HOME}/.antidote/antidote.zsh
+PYENV = ${HOME}/.pyenv/bin/pyenv
+
 setup: setup-zsh
 
 setup-zsh: $(ANTIDOTE) $(PURE) $(PYENV) $(ZSH) link-zsh-env link-zsh-plugins
@@ -6,17 +11,14 @@ setup-zsh: SH_NAME=$(shell which zsh)
 setup-zsh: 
 	$(call maybesudo, chsh -s $(SH_NAME) $(shell whoami))
 
-ZSH = $(shell which zsh)
 $(ZSH): $(PACAPT)
 	$(call pacapt, install zsh)
 
-PURE = $(shell which pure)
 $(PURE):
 	mkdir -p ${HOME}/.zsh
 	git clone https://github.com/sindresorhus/pure.git ${HOME}/.zsh/pure
 
-ANTIDOTE = ${HOME}/.antidote/antidote.zsh
-ANTIDOTE:
+$(ANTIDOTE):
 	git clone https://github.com/mattmc3/antidote.git ${HOME}/.antidote
 
 link-zsh-env:
@@ -33,6 +35,5 @@ link-zsh-plugins:
 	$(call backup, ${HOME}/.zsh_plugins.txt)
 	ln -s -f ${CURDIR}/.zsh_plugins.txt ${HOME}
 
-PYENV = $(shell which pyenv)
 $(PYENV):
 	curl https://pyenv.run | bash
