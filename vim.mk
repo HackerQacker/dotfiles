6d7b306e824d38c08a2114982dad1cd95a78c210
HackerQacker <omer@yehudai.me>
2023-07-19 10:43:39 +0300

    Changed vim.mk:

--- vim.mk
+++ vim.mk
@@ -1,10 +1,8 @@
 
-NVIM_PLUG=${HOME}/.local/share/nvim/site/autoload/plug.vim
 NVIM_CONFIG=${HOME}/.config/nvim
 
 setup: setup-nvim
 
-setup-nvim: install-yarn install-nvim install-pynvim $(NVIM_PLUG) link-nviminit install-plugins
 setup-nvim: install-yarn install-nvim install-pynvim $(NVIM_CONFIG)
 setup-nvim: link-ideavim
 
@@ -19,22 +17,12 @@ install-pynvim:
 	$(call pacapt, install python3)
 	python3 -m pip install pynvim
 
-link-nviminit: $(NVIM_CONFIG)
-	$(call backup, ${NVIM_CONFIG}/init.vim})
-	ln -s -f ${CURDIR}/init.vim $(NVIM_CONFIG)
-
-link-nviminit-lua: 
-	# $(call backup, ${NVIM_CONFIG}/init.vim})
-
 link-ideavim:
 	ln -s -f ${CURDIR}/.ideavimrc ${HOME}
 
 install-plugins:
 	nvim --headless +'PlugInstall --sync' +'PlugUpdate' +'qall'
 	nvim --headless +'CocInstall -sync' +'CocUpdateSync|q'
-
-$(NVIM_PLUG):
-	curl -fLo $@ --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 $(NVIM_CONFIG):
 	ln -s -f ${CURDIR}/nvim $(NVIM_CONFIG)
