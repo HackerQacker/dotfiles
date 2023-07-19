51c544dc7337f767e94f438210cbd7d35f1ab4b2
HackerQacker <omer@yehudai.me>
2023-07-19 10:37:38 +0300

    Fixed markdown preview

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,44 +1,46 @@
--- TODO: Fucking fix the shit below
-function Open_chrome_in_new_popup(url)
-	vim.fn.system({
-		'open',
-		'-na',
-		'"Google Chrome"',
-		'--args',
-		'--app=' .. url,
-	})
-end
 
-function Open_mac_chromium_in_new_popup(url)
-	vim.fn.system({
-		'open',
-		'-na',
-		'"Chromium"',
-		'--args',
-		'--app',
-		url,
-	})
-end
-
-vim.cmd(
-	[[
-function Open_mac_chrome_in_new_popup(url)
-    exe 'silent !open -na "Google Chrome" --args --app=' . a:url
-endfunction
-]]
-)
-if vim.fn.has('macunix') then
-	-- Uhh! that's dirty
-	vim.g.mkdp_browserfunc = 'Open_mac_chrome_in_new_popup'
-end
+-- function Open_chrome_in_new_popup(url)
+-- 	vim.fn.system({
+-- 		'open',
+-- 		'-na',
+-- 		'"Google Chrome"',
+-- 		'--args',
+-- 		'--app=' .. url,
+-- 	})
+-- end
+--
+-- function Open_mac_chromium_in_new_popup(url)
+-- 	vim.fn.system({
+-- 		'open',
+-- 		'-na',
+-- 		'"Chromium"',
+-- 		'--args',
+-- 		'--app',
+-- 		url,
+-- 	})
+-- end
 
 return {
 	{
 		'iamcco/markdown-preview.nvim',
-		build = "cd app && npm install",
 		ft = "markdown",
+		-- build = "cd app && npm install",
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
 		lazy = true,
 		config = function()
+			vim.cmd(
+				[[
+		function Open_mac_chrome_in_new_popup(url)
+		    exe 'silent !open -na "Google Chrome" --args --app=' . a:url
+		endfunction
+		]]
+			)
+			if vim.fn.has('macunix') then
+				-- Uhh! that's dirty
+				vim.g.mkdp_browserfunc = 'Open_mac_chrome_in_new_popup'
+			end
 		end
 	}
 }
