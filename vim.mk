64625c54b95bf3d7ece8a40b2fad75935f222906
HackerQacker <omeryehudai@gmail.com>
2023-07-20 19:06:03 +0300

    Added necessary nvm,node installation

--- vim.mk
+++ vim.mk
@@ -1,15 +1,23 @@
 
+
+# TODO: Add fzf
+
 NVIM_CONFIG=${HOME}/.config/nvim
 
 setup: setup-nvim
 
-setup-nvim: install-yarn install-nvim install-pynvim link-configs
+setup-nvim: install-nvm install-npm install-yarn install-nvim install-pynvim link-configs
 setup-nvim: link-ideavim
 
 install-nvim: $(PACAPT)
 	$(call pacapt, install neovim)
 
-# Reuired for some plugins
+install-nvm:
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+install-npm: $(shell which nvm)
+	nvm install node
+
 install-yarn: $(PACAPT)
 	$(call pacapt, install yarn)
 
